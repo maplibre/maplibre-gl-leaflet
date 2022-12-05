f8c982b72ba2dc7ef191902b9ab69fcdd687817f
Luke Seelenbinder <me@lukeseelenbinder.com>
2022-12-05 12:19:50 +0900

    Also namespace MaplibreGL.Map in .d.js.

--- leaflet-maplibre-gl.d.ts
+++ leaflet-maplibre-gl.d.ts
@@ -1,12 +1,12 @@
 import * as L from 'leaflet';
-import { Map, MapOptions as MaplibreGLOptions } from 'maplibre-gl';
+import { Map as MaplibreGLMap, MapOptions as MaplibreGLOptions } from 'maplibre-gl';
 
 declare module 'leaflet' {
     type LeafletMaplibreGLOptions = Omit<MaplibreGLOptions, "container">;
 
     class MaplibreGL extends L.Layer {
         constructor(options: LeafletMaplibreGLOptions);
-        getMaplibreMap(): Map
+        getMaplibreMap(): MaplibreGLMap
         getCanvas(): HTMLCanvasElement
         getSize(): L.Point
         getBounds(): L.LatLngBounds
